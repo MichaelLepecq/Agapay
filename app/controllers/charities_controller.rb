c63d5040cfdc26f42819610f000dee25d9af16d4
mg ayoub <mg.ayoub@lewagon.org>
2018-05-31 14:48:39 -0400

    favorite in and out, card css

--- app/controllers/charities_controller.rb
+++ app/controllers/charities_controller.rb
@@ -28,16 +28,20 @@ class CharitiesController < ApplicationController
     # verifier si ya un favorite pour cete charite
     # si oui alors je la detruit
     # si non alors je la cree
+
     @charity = Charity.find(charity_params[:charity_id])
 
-    # if favorite[:charity_id].present?
+    @favorite = UserCharity.where(user: current_user, charity: @charity)
+
+    if @favorite.empty?
       @favorite = UserCharity.create(user: current_user, charity: @charity)
-    # else
-    #   @favorite.destroy
-    # end
+    else
+      @favorite.first.destroy
+    end
   end
 
   private
+
   def charity_params
     params.permit(:charity_id)
   end
