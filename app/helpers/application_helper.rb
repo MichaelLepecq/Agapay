3dffe8972425885621a847ebce174627d3b8a76f
mg ayoub <mg.ayoub@lewagon.org>
2018-06-04 17:17:09 -0400

    drawer, signin, menu, signup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,16 @@ module ApplicationHelper
     return File.read(file_path).html_safe if File.exists?(file_path)
     '(not found)'
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+   @devise_mapping ||= Devise.mappings[:user]
+  end
 end
