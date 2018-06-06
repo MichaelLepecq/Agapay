4d08900025910c12fe3ed268fa7ab128fffb546e
Michael Lepecq <lepecqmichael@gmail.com>
2018-06-06 17:53:21 -0400

    change path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   config.assets.compile = true
+  config.assets.enabled = true
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
