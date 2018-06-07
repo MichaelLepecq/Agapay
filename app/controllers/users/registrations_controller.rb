80d4d931ba4cff9b22dd3de135888ef71ae0713e
Gaelle Londoz <gaelle.londoz@gmail.com>
2018-06-07 14:29:51 -0400

    fin corr

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -38,7 +38,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
   def user_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :photo)
   end
 
 
