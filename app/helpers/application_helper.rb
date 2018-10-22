3290acef0f884a0d327c00b49b310bd41c88fd53
Michael Lepecq <lepecqmichael@gmail.com>
2018-10-22 09:07:36 -0400

    upgraded dependencies

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,4 @@
-module ApplicationHelper
+   module ApplicationHelper
   def inline_svg(name)
     file_path = "#{Rails.root}/app/assets/images/svg/#{name}.svg"
     return File.read(file_path).html_safe if File.exists?(file_path)
--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -24,7 +24,6 @@ SimpleForm.setup do |config|
     # Determines whether to use HTML5 (:email, :url, ...)
     # and required attributes
     b.use :html5
-
     # Calculates placeholders automatically from I18n
     # You can also pass a string as f.input placeholder: "Placeholder"
     b.use :placeholder
